67ad629e362b2adda3f1225d2e0a233d72af8339
miyling <56378115+miyling@users.noreply.github.com>
2023-04-04 18:26:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,26 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 师夷长技以制夷
+git clone https://github.com/xiaorouji/openwrt-passwall -b packages package/passwall_packages
+git clone https://github.com/xiaorouji/openwrt-passwall -b luci-smartdns-new-version package/luci-app-passwall
+
+# ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+
+# smartdns
+git clone https://github.com/pymumu/luci-app-smartdns -b lede package/luci-app-smartdns
+
+# ad home
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+# watchcat-plus
+git clone https://github.com/gngpp/luci-app-watchcat-plus package/luci-app-watchcat-plus
+
+# 晶晨宝盒
+git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+
+# feeds up
+./scripts/feeds update -a
+./scripts/feeds install -a
