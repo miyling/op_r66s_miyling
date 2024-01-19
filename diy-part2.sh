a2e4376b83cb464852125882cbbfa7e2743d875f
miyling <56378115+miyling@users.noreply.github.com>
2024-01-19 15:14:07 +0800

    feat: update clone function

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,25 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
@@ -31,7 +50,7 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 
 # watchcat-plus
 rm -rf feeds/packages/utils/watchcat
-svn co https://github.com/openwrt/packages/trunk/utils/watchcat feeds/packages/utils/watchcat
+merge_package master https://github.com/openwrt/packages feeds/packages/utils utils/watchcat
 git clone https://github.com/gngpp/luci-app-watchcat-plus package/luci-app-watchcat-plus
 
 # 晶晨宝盒
